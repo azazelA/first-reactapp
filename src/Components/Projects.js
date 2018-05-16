4e12c46727636cd29e7cbcdd85162bbcf568680c
ahmad <ahmadhabib@punch.cool>
2018-05-16 06:41:06 +0500

    added timer example to explain states and props

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        // State is always defined inside the constructor
+        this.state = {date: new Date()};
+    }
+    // Lifecycle Hooks
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    tick() {
+        // Set State
+        this.setState({
+            date: new Date()
+        });
+    }
     render(){
         return (
             <div className="Projects">
-                My Projects
+                <h1>My Timer</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         )
     }
