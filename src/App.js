27e8427714e452bd53ed657f89daa48fe218bb52
ahmad <ahmadhabib@punch.cool>
2018-05-16 06:17:18 +0500

    initial commit along with a new component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
+
+class Smk extends Component {
+    render(){
+        return (
+            <div className="App">
+                <p>My App</p>
+                <Projects/>
+            </div>
+        )
+    }
+}
+// export default App;
+export default Smk;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Smk from './App.js';
+import registerServiceWorker from './registerServiceWorker';
+
+
+ReactDOM.render(<Smk/>, document.getElementById('root'));
+registerServiceWorker();
