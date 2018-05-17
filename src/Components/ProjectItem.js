9f07774c20aea47d04b0bb6a6205ddf51b35ef16
ahmad <ahmadhabib@punch.cool>
2018-05-17 07:31:15 +0500

    passing uuid to onDelete function

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 class ProjectItem extends Component {
+    deleteProject (id){
+        this.props.onDelete(id);
+    }
     render(){
-
         return (
             <li className="ProjectItem">
                 <h4><small>Title:</small> {this.props.project.title}</h4>
                 <p><small>Category:</small> {this.props.project.category}</p>
+                <button className="delete" onClick={this.deleteProject.bind(this, this.props.project.id)}>x</button>
             </li>
         )
     }
