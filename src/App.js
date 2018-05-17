ae4ad1f31c9ef55055ae31f4505c56f53473d6f8
ahmad <ahmadhabib@punch.cool>
2018-05-17 07:18:47 +0500

    added uuid to default projects state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 import './App.css'
 import Projects from './Components/Projects';
 import logo from './logo.svg';
@@ -16,26 +17,33 @@ class Smk extends Component {
         this.setState({
             projects: [
                 {
+                    id: uuid.v4(),
                     title: "Video Surveillance System",
                     category: "Security"
                 },
                 {
+                    id: uuid.v4(),
                     title: "Live Leads",
                     category: "B2B lead generation"
                 },
                 {
+                    id: uuid.v4(),
                     title: "Road Navigator",
                     category: "Directions & Navigation"
                 }
             ]
         })
     }
-
+    handleAddProject(newProject){
+        let currentProjects = this.state.projects;
+        currentProjects.push(newProject);
+        this.setState({projects: currentProjects});
+    }
     render(){
         return (
             <div className="App">
                 <h1>Self Projects <span><img src={logo} alt="Self Projects"/></span></h1>
-                <AddProject />
+                <AddProject addProject={this.handleAddProject.bind(this)}/>
                 <Projects projects={this.state.projects}/>
 
             </div>
